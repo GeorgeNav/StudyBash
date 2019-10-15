1c0101b56e0ffc03e15aa776232707166c3f26d9
GeorgeNav <george.g.navarro@gmail.com>
2019-10-14 22:21:47 -0500

    fixed crash issue because of database changes

--- studybash/Views/Main/Goals/GoalsViewController.swift
+++ studybash/Views/Main/Goals/GoalsViewController.swift
@@ -10,27 +10,12 @@ import UIKit
 import Firebase
 import FirebaseFirestore
 
-struct UserData {
-    var info: [String: Any]
-    var goals: [String: Any]
-
-    init() {
-        info = ["":""]
-        goals = ["":""]
-    }
-
-    init(info i:[String: Any], goals g:[String: Any]) {
-        info = i
-        goals = g
-    }
-}
-
 class GoalsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     @IBOutlet weak var goalsCV: UICollectionView!
     let goalsCellIdentifier: String = "goal_cell"
     let db: Firestore = Firestore.firestore()
     var allGoals: [String] = [String]()
-    var ud: UserData = UserData()
+    var uid: String = ""
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return allGoals.count
@@ -52,58 +37,23 @@ class GoalsViewController: UIViewController, UICollectionViewDataSource, UIColle
         self.view.addSubview(goalsCV)
         self.goalsCV.dataSource = self
         self.goalsCV.delegate = self
-        
+        uid = Auth.auth().currentUser!.uid
         getUserData(uid: "schema")
     }
 
     func getUserData(uid:String) {
-        var info: [String: Any] = [String: Any]()
-        var goals: [String: Any] = [String: Any]()
-        db.collection("users").document(uid).getDocument() {(snapshot:DocumentSnapshot?, error:Error?) in
-            guard snapshot != nil else { print("Error: \(error!)") ; return }
-            info = snapshot!.data()!
-            let schemaGoalsRef: DocumentReference = snapshot!.data()!["goals"]! as! DocumentReference
-            schemaGoalsRef.getDocument() {(snapshot, error) in
-                guard snapshot != nil else { print("Error: \(error!)") ; return }
-                goals = snapshot!.data()!
-                self.ud = UserData(info: info, goals: goals)
-                print(self.ud.info)
-                print(self.ud.goals)
+        db.collection("users").document(uid).collection("goals").getDocuments(completion: { (goalDocRefs, error) in
+            goalDocRefs?.documents.forEach({ (doc) in
+                let goalData = doc.data()
+                self.allGoals.append(goalData["name"]! as! String)
                 self.goalsCV.reloadData()
-                print("Goal keys: ", Array(self.ud.goals.keys))
-                print("Goals: ", self.allGoals)
-                
-                //self.subGoalsDueOnDate(year: 2019, month: 10, day: 13)
-            }
-        }
+            })
+        })
+        
+        //subGoalsDueOnDate(year: 2019, month: 10, day: 14)
     }
     
     func subGoalsDueOnDate(year:Int, month:Int, day: Int) {
-        var allSubGoalsDueThisDay: [String: Any] = [String: Any]()
-        var dateComponents = DateComponents()
-        dateComponents.year = year
-        dateComponents.month = month
-        dateComponents.day = day
-        
-        let userCalendar = Calendar.current
-        let date = userCalendar.date(from: dateComponents)!
-        
-        self.ud.goals.forEach({(goalKey, goalData) in
-            let gd = goalData as! [String: Any]
-            self.allGoals.append(gd["name"]! as! String)
-            let subGoalsRef = gd["sub_goals"]! as! DocumentReference
-            print(gd["name"]! as! String)
-            subGoalsRef.getDocument() {(snapshot, error) in
-                guard snapshot != nil else { print("Error: \(error!)") ; return }
-                let subGoalsData = snapshot!.data()!
-                subGoalsData.forEach({(subGoalKey:String, subGoalData:Any) in
-                    let data = subGoalData as! [String: Any]
-                    let dueDate = data["due_date"] as! Timestamp
-                    if(Calendar.current.dateComponents([.day], from: dueDate.dateValue(), to: date).day == 0) {
-                        allSubGoalsDueThisDay[subGoalKey] = data
-                    }
-                })
-            }
-        })
+        db.collection("users").document(uid).collection("goals")
     }
 }
