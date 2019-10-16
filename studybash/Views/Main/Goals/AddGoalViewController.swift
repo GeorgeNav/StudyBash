fc33ac9bdfad69d8e71df328bf32703c105d8892
GeorgeNav <george.g.navarro@gmail.com>
2019-10-16 12:37:09 -0500

    working on AddGoal view controller

---
+++ studybash/Views/Main/Goals/AddGoalViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddGoalViewController.swift
+//  studybash
+//
+//  Created by George Navarro on 10/15/19.
+//  Copyright © 2019 Navality. All rights reserved.
+//
+
+import UIKit
+
+class AddGoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func createGoalButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+        performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
+    }
+}
