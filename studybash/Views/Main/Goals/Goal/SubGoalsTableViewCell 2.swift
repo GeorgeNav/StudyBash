e47bf739a13265a3c533d7c0b175a4ccbdefbaca
Mustafa AL-Jaburi <maljaburi@gmail.com>
2019-10-19 23:35:46 -0500

    -

---
+++ studybash/Views/Main/Goals/Goal/SubGoalsTableViewCell 2.swift
@@ -0,0 +1,25 @@
+//
+//  SubGoalsTableViewCell.swift
+//  studybash
+//
+//  Created by George Navarro on 10/17/19.
+//  Copyright © 2019 Navality. All rights reserved.
+//
+
+import UIKit
+
+class SubGoalsTableViewCell: UITableViewCell {
+    @IBOutlet weak var subGoalName: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
