38d7c3589cc656247a3f207c312b435ee693e1c7
Mustafa <mmq7000@gmail.com>
2019-11-05 23:39:45 -0600

    push

---
+++ studybash/Views/Onboarding/LoginViewController 2.swift
@@ -0,0 +1,53 @@
+//
+//  LoginViewController.swift
+//  studybash
+//
+//  Created by George Navarro on 10/11/19.
+//  Copyright © 2019 Navality. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    var auth: Auth = Auth.auth()
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+
+    @IBAction func signInButton(_ sender: Any) {
+        guard emailTF.text?.count != 0 || passwordTF.text?.count != 0 else { return }
+        let email: String = emailTF.text!
+        let password: String = passwordTF.text!
+
+        auth.signIn(withEmail: email, password: password, completion: { (authResult, error) in
+            guard authResult != nil else {
+                print("Error: \(error!)")
+                return
+            }
+            print(authResult!.user.email!, " is logged in!")
+            self.performSegue(withIdentifier: "sign_in_to_goals", sender: self)
+        })
+    }
+
+    @IBAction func signInWithAppleButton(_ sender: Any) { // TODO: implement apple signin
+    }
+    
+    func signInRelatedStuff() {
+//        auth.currentUser?.delete(completion: { (error) in
+//            guard error != nil else { return }
+//            print("Error: \(error!)")
+//        })
+    }
+    
+}
