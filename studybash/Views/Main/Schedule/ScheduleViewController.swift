ae7a0d27a2cb44d86f59bb9ab8a34ec63015720b
GeorgeNav <george.g.navarro@gmail.com>
2019-11-15 13:52:20 -0600

    schedule view database logic for selecting a day and getting the correct data (complete)

--- studybash/Views/Main/Schedule/ScheduleViewController.swift
+++ studybash/Views/Main/Schedule/ScheduleViewController.swift
@@ -10,9 +10,11 @@ import FSCalendar
 class ScheduleViewController: UIViewController {
     let db: Firestore = Firestore.firestore()
     @IBOutlet weak var calendar: FSCalendar!
+    var calendarData = [String: Any]()
+    var calendarDayListeners = [String: ListenerRegistration]()
+    
     var currentMonthPos = -1
     
-    var subGoalsDataForMonths = [String: [String: Any]]()
     var currentDaySubGoalsListener: ListenerRegistration?
     var userDocRef: DocumentReference?
     
@@ -22,19 +24,17 @@ class ScheduleViewController: UIViewController {
     }
     
     func subGoalsDueOnDate(date: Date) {
-        
+        var allSubGoalsDueThisDay = [String]()
         self.db.collectionGroup("sub_goals")
         .whereField("due_date", onThisDay: date)
         .whereField("uid_ref", isEqualTo: userDocRef!)
         .getDocuments(completion: { (snapshot, error) in
             guard snapshot != nil else { print(error!); return }
-            var allSubGoals = [String]()
             snapshot?.documents.forEach({ (subGoalDocRef) in
                 let subGoalData = subGoalDocRef.data()
                 //print((subGoalData["due_date"]! as! Timestamp), " - ", subGoalData["name"]!, " - query")
-                allSubGoals.append(subGoalData["name"]! as! String)
+                allSubGoalsDueThisDay.append(subGoalData["name"]! as! String)
             })
-            print(allSubGoals)
         })
     }
     
@@ -42,15 +42,35 @@ class ScheduleViewController: UIViewController {
 
 extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegate {
     public func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        let f2 = DateFormatter()
-        f2.dateFormat = "MM/dd/yyyy HH:mm:ss"
-        print(f2.string(from: date))
-        subGoalsDueOnDate(date: date)
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "MM/dd/yyyy"
+        if calendarData[dateFormat.string(from: date)] != nil { // Do something with the data
+            dump(calendarData[dateFormat.string(from: date)]!)
+        }
     }
     
     public func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
         let cell = calendar.dequeueReusableCell(withIdentifier: "schedule_cal_cell", for: date, at: position)
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "MM/dd/yyyy"
+        dateFormat.string(from: date)
+        
         cell.imageView.contentMode = .scaleAspectFit
+        if calendarDayListeners[dateFormat.string(from: date)] == nil {
+            calendarDayListeners[dateFormat.string(from: date)] = db.collectionGroup("sub_goals")
+            .whereField("due_date", onThisDay: date)
+            .whereField("uid_ref", isEqualTo: userDocRef!)
+            .addSnapshotListener({ (snapshot, error) in
+                guard snapshot != nil || snapshot!.count == 0 else { return }
+                var allSubGoalsDueThisDay = [[String: Any]]()
+                snapshot!.documents.forEach({ (subGoalDocRef) in
+                    //print((subGoalData["due_date"]! as! Timestamp), " - ", subGoalData["name"]!, " - query")
+                    allSubGoalsDueThisDay.append(subGoalDocRef.data())
+                })
+                self.calendarData[dateFormat.string(from: date)] = allSubGoalsDueThisDay
+                calendar.reloadData()
+            })
+        }
         
         return cell
     }
@@ -72,33 +92,3 @@ extension Query {
         return whereField("due_date", isGreaterThanOrEqualTo: startTimestamp).whereField("due_date", isLessThan: endTimestamp)
     }
 }
-
-/*
- @IBOutlet weak var circleV: UIView!
- 
- func DrawCircle(progress: Double) {
-     guard progress >= 0 && progress <= 1 else { return }
-     
-     let circleCenter = circleV.center
-     
-     let circlePath = UIBezierPath(arcCenter: circleCenter, radius: (circleV.bounds.width/2 - 5), startAngle: -CGFloat.pi/2, endAngle: (2 * CGFloat.pi), clockwise: true)
-     
-     let CircleLayer = CAShapeLayer()
-     CircleLayer.path = circlePath.cgPath
-     CircleLayer.strokeColor = UIColor.red.cgColor
-     CircleLayer.lineWidth = 2
-     CircleLayer.strokeEnd = 0
-     CircleLayer.fillColor = UIColor.clear.cgColor
-     CircleLayer.lineCap = CAShapeLayerLineCap.round
-     
-     let Animation = CABasicAnimation(keyPath: "strokeEnd")
-     Animation.duration = 1
-     Animation.toValue = progress
-     Animation.fillMode = CAMediaTimingFillMode.forwards
-     Animation.isRemovedOnCompletion = false
-     
-     CircleLayer.add(Animation, forKey: nil)
-     circleV.layer.addSublayer(CircleLayer)
-     circleV.layer.backgroundColor = UIColor.clear.cgColor
- }
- */
